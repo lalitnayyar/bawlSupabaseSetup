8b3111c53b00f4f92c617bfc97423ae53239ad3a
Lalit <lalitnayyar@gmail.com>
2025-01-01 03:55:07 +0400

    splittext2

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { promises as fs } from 'fs'
 
 try {
-  const result = await fetch('scrimba-info.txt')
-  const text = await result.text()
+  const text = await fs.readFile('scrimba-info.txt', 'utf-8')
+  
+  const splitter = new RecursiveCharacterTextSplitter()
+  
+  const output = await splitter.createDocuments([text])
+  console.log(output)
 } catch (err) {
   console.log(err)
 }
